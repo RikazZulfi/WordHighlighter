3d1074665f631f4a4c80dd743c1ce5e5741bdd80
RikazZulfi <rikazzulfi@gmail.com>
2024-11-04 07:08:34 +0530

    content.js

--- content.js
+++ content.js
@@ -1,15 +1,23 @@
-let wordsList = [];
-
-chrome.storage.sync.get('words', (data) => {
-  wordsList = data.words;
-  highlightWords();
-});
-
 function highlightWords() {
-  const bodyText = document.body.innerHTML;
-  wordsList.forEach(({ word, color }) => {
-    const regex = new RegExp(`\\b(${word})\\b`, 'gi');
-    const newText = bodyText.replace(regex, `<span class="highlighted" style="background-color: ${color};" title="${word}">$1</span>`);
-    document.body.innerHTML = newText;
-  });
+  const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
+  const words = wordsList.map(({ word, color }) => ({
+    regex: new RegExp(`\\b(${word})\\b`, 'gi'),
+    color,
+  }));
+
+  let node;
+  while (node = walker.nextNode()) {
+    let text = node.nodeValue;
+    let newHTML = text;
+
+    words.forEach(({ regex, color }) => {
+      newHTML = newHTML.replace(regex, `<span class="highlighted" style="background-color: ${color};" title="$1">$1</span>`);
+    });
+
+    if (newHTML !== text) {
+      const span = document.createElement('span');
+      span.innerHTML = newHTML;
+      node.parentNode.replaceChild(span, node);
+    }
+  }
 }
